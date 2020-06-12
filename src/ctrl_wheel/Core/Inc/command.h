b997b8de1e696ba850ab611dd444e51934e15174
Daniel <danielpth@gmail.com>
2020-06-11 21:04:28 -0300

    Improve command functions

--- src/ctrl_wheel/Core/Inc/command.h
+++ src/ctrl_wheel/Core/Inc/command.h
@@ -1,7 +1,7 @@
 #ifndef __COMMAND__
 #define __COMMAND__
 
-#include "main.h"
+#include <stdint.h>
 
 #define COMMAND_JUMP_BOOTLOADER 1
 #define COMMAND_TURNON_SPEED_CONTROL 2
@@ -13,6 +13,26 @@
 #define COMMAND_SET_I 8
 #define COMMAND_SET_D 9
 #define COMMAND_CLEAR_POSITION 10
+
+#define STATE_START_FRAME 0
+#define STATE_GET_LENGHT 1
+#define STATE_GET_PAYLOAD 2
+#define STATE_GET_CRC 3
+#define STATE_DONE 4
+
+#define COMMAND_SOF 2
+#define COMMAND_EOF 3
+
+struct __attribute__((__packed__)) s_command_status {
+  uint8_t cmd;
+  uint32_t timestamp;
+  double position1;
+  double position2;
+  double speed1;
+  double speed2;
+  uint16_t bat;
+  uint8_t result;
+};
 
 void CommandControl(void);
 
--- src/ctrl_wheel/Core/Src/command.c
+++ src/ctrl_wheel/Core/Src/command.c
@@ -1,4 +1,5 @@
 #include "command.h"
+#include "main.h"
 #include "motor.h"
 #include "flash.h"
 #include "adc.h"
@@ -11,16 +12,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct s_command_status {
-  uint32_t timestamp;
-  int32_t position1;
-  int32_t position2;
-  double speed1;
-  double speed2;
-  uint16_t bat;
-} command_status;
+struct s_command_status command_status;
 
-void CommandJumpBootloader(void) {
+static uint8_t CommandReceive(void *payload, uint8_t size_of_payload) {
+  uint8_t data;
+  uint8_t state = STATE_START_FRAME;
+  uint8_t length, index = 0;
+  uint8_t *buffer = payload;
+  HAL_StatusTypeDef status;
+  uint8_t result = HAL_ERROR;
+
+  do {
+    status = HAL_UART_Receive(&huart1, &data, 1, 1);
+
+    if (status == HAL_OK) {
+      switch (state) {
+      case STATE_START_FRAME:
+        if (data == COMMAND_SOF)
+          state = STATE_GET_LENGHT;
+        break;
+
+      case STATE_GET_LENGHT:
+        length = data;
+        if (length > size_of_payload) {
+          state = STATE_START_FRAME;
+          status = HAL_ERROR;
+        } else {
+          state = STATE_GET_PAYLOAD;
+        }
+        break;
+
+      case STATE_GET_PAYLOAD:
+        buffer[index] = data;
+        index++;
+        if (index == length)
+          state = STATE_DONE;
+        break;
+      }
+    } else {
+      result = status;
+    }
+  } while ((status == HAL_OK) && (state != STATE_DONE));
+
+  if (state == STATE_DONE) {
+    //CommandEchoBack(COMMAND_SOF);
+    result = HAL_OK;
+  }
+
+  return result;
+}
+
+static void CommandTransmit(void *payload, uint8_t lenght) {
+  uint8_t data = COMMAND_SOF;
+  uint8_t *buffer = payload;
+  HAL_UART_Transmit(&huart1, &data, 1, 10);
+  HAL_UART_Transmit(&huart1, &lenght, 1, 10);
+  HAL_UART_Transmit(&huart1, buffer, lenght, lenght + 1);
+}
+
+static void CommandJumpBootloader(void) {
   HAL_TIM_Base_Stop_IT(&htim1);
   HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_1);
   HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_2);
@@ -33,60 +83,65 @@ void CommandJumpBootloader(void) {
   flash_jump_to_app();
 }
 
-void CommandEchoBack(uint8_t echo) {
-  // Send echo back indicating that command was received
-  HAL_UART_Transmit(&huart1, &echo, 1, 100);
-}
+static void CommandSendStatus(uint8_t r) {
 
-void SendStatus(void) {
-  //sprintf((char*)buffer, "R: %08x %04x %08x %08x %05x %05x\n\r", command_status.timestamp, command_status.bat, command_status.position1,
-      //command_status.position2, (int) (command_status.speed1 * 1000.0), (int) (command_status.speed1 * 1000.0));
-
-}
-
-void CommandControl(void) {
-  HAL_StatusTypeDef status;
-  uint8_t command;
   // Get status atomic as possible to send to host
+  command_status.cmd = 'S';
   command_status.timestamp = HAL_GetTick();
   command_status.bat = BATTERY;
   command_status.position1 = motor1_position;
   command_status.position2 = motor2_position;
   command_status.speed1 = motor1_speed;
   command_status.speed2 = motor2_speed;
-  HAL_UART_Transmit(&huart1, (uint8_t*) &command_status, sizeof(command_status), 100);
+  command_status.result = r;
+  CommandTransmit(&command_status, sizeof(command_status));
+}
 
-  // Get command by serial
-  status = HAL_UART_Receive(&huart1, &command, 1, 10);
+static void CommandSetVoltage(uint8_t *buff) {
+  double *voltage1, *voltage2;
+  voltage1 = (double *)&buff[0];
+  voltage2 = (double *)&buff[sizeof(double)];
+  Motor1SetVoltage(*voltage1);
+  Motor2SetVoltage(*voltage2);
+}
+
+static void CommandSetSpeed(uint8_t *buff) {
+  double *speed1, *speed2;
+  speed1 = (double *)&buff[0];
+  speed2 = (double *)&buff[sizeof(double)];
+  Motor1SetReference(*speed1);
+  Motor2SetReference(*speed2);
+}
+
+void CommandControl(void) {
+  uint8_t status = 0, result = 0;
+  uint8_t buf[14];
+
+  // Get command
+  status = CommandReceive(buf, sizeof(buf));
 
   if (status == HAL_OK) {
-    switch (command) {
+    result = 1;
+    switch (buf[0]) {
 
     case COMMAND_JUMP_BOOTLOADER:
-      CommandEchoBack(COMMAND_JUMP_BOOTLOADER);
       CommandJumpBootloader();
       break;
 
     case COMMAND_TURNON_SPEED_CONTROL:
-      CommandEchoBack(COMMAND_TURNON_SPEED_CONTROL);
       MotorTurnOnControlSpeed();
       break;
 
     case COMMAND_TURNOFF_SPEED_CONTROL:
-      CommandEchoBack(COMMAND_TURNOFF_SPEED_CONTROL);
       MotorTurnOffControlSpeed();
       break;
 
     case COMMAND_SET_VOLTAGE:
-      CommandEchoBack(COMMAND_SET_VOLTAGE);
-      Motor1SetVoltage(0);
-      Motor2SetVoltage(0);
+      CommandSetVoltage(&buf[1]);
       break;
 
     case COMMAND_SET_SPEED:
-      CommandEchoBack(COMMAND_SET_SPEED);
-      Motor1SetReference(0);
-      Motor2SetReference(0);
+      CommandSetSpeed(&buf[1]);
       break;
 
     case COMMAND_SET_POSITION:
@@ -102,10 +157,16 @@ void CommandControl(void) {
       break;
 
     case COMMAND_CLEAR_POSITION:
-      CommandEchoBack(COMMAND_CLEAR_POSITION);
       MotorClearPosition();
       break;
 
+    default:
+      result = 2;
+      break;
+
     }
+  } else if (status == HAL_TIMEOUT) {
+    result = 0;
   }
+  CommandSendStatus(result);
 }
