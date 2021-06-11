1acbc1bd4147e0136b8738657e995c3a9fcb6a71
Daniel <danielpth@gmail.com>
2021-06-11 09:56:03 -0300

    Configure vision3d project to build remote in Ubuntu

--- src/robotd/vision3d/main.cpp
+++ src/robotd/vision3d/main.cpp
@@ -1,7 +1,59 @@
 #include <cstdio>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+
+//#include "opencv2/features2d/features2d.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/nonfree/nonfree.hpp"
+
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
     printf("hello from %s!\n", "vision3d");
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    VideoCapture cap(0);
+
+    setenv("DISPLAY", ":1", 1);
+
+    // Check if camera opened successfully
+    if (!cap.isOpened()) {
+        cout << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    while (1) {
+        Mat frame;
+        // Capture frame-by-frame
+        cap >> frame;
+
+        // If the frame is empty, break immediately
+        if (frame.empty())
+            break;
+
+        // Display the resulting frame
+        imshow("Frame", frame);
+
+        // Press  ESC on keyboard to exit
+        char c = (char)waitKey(25);
+        if (c == 27)
+            break;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    destroyAllWindows();
+
     return 0;
 }
